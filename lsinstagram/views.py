9c4bbde1676b85680357a789b80ade68379546b2
xlnls <1300152526@qq.com>
2017-05-23 11:17:04 +0800

    项目起始

--- lsinstagram/views.py
+++ lsinstagram/views.py
@@ -1,7 +1,10 @@
 # -*- coding=utf-8 -*-
 
 from lsinstagram import app
-
+from flask import render_template
+from models import Image,User
 @app.route('/')
 def index():
-    return 'hello'
+    image  = Image.query()
+
+    return render_template('index.html')
